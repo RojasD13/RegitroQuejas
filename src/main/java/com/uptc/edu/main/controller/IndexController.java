f2e17d33774f846e4e5807cf90229cfb1150db97
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2025-08-21 15:29:11 -0500

    feat: add index redirect

---
+++ src/main/java/com/uptc/edu/main/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.uptc.edu.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    
+    
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "redirect:registro";
+    }
+}
