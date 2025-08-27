7a8c63aed9606ffa501933486a970e2a01f87b87
Yesid Martinez <yesid.martinez04@uptc.edu.co>
2025-08-27 17:18:59 -0500

    Mensaje de bug de salto de pagina arreglado

--- src/main/java/com/uptc/edu/main/model/Queja.java
+++ src/main/java/com/uptc/edu/main/model/Queja.java
@@ -12,9 +12,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+
 @Entity
 @Table(name = "quejas")
-
 public class Queja {
 
     @Id
