8a2e29bd79d0ab2929c4746b196c06b14251e7a9
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2025-09-12 18:10:04 -0500

    refactor: uso de Lombok en EmpresaResumenDTO.java

--- src/main/java/com/uptc/edu/main/dto/EmpresaResumenDTO.java
+++ src/main/java/com/uptc/edu/main/dto/EmpresaResumenDTO.java
@@ -1,37 +1,14 @@
 package com.uptc.edu.main.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter
+@Setter
 public class EmpresaResumenDTO {
+    
     private Long id;
     private String nombreEmpresa;
     private long totalQuejas;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombreEmpresa() {
-        return nombreEmpresa;
-    }
-
-    public void setNombreEmpresa(String nombreEmpresa) {
-        this.nombreEmpresa = nombreEmpresa;
-    }
-
-    public long getTotalQuejas() {
-        return totalQuejas;
-    }
-
-    public void setTotalQuejas(long totalQuejas) {
-        this.totalQuejas = totalQuejas;
-    }
-
-    
 }
-
-    
