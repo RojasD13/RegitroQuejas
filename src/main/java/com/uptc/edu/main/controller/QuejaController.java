6c1791585c5c85ed585d128aa8c0049814685cd9
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2025-09-12 19:23:17 -0500

    refactor: definición de PatchMapping en el enpoint ```/quejas/{id}/ocultar```

--- src/main/java/com/uptc/edu/main/controller/QuejaController.java
+++ src/main/java/com/uptc/edu/main/controller/QuejaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -81,7 +82,7 @@ public class QuejaController {
         return "buscar";
     }
 
-    @PostMapping("/quejas/{id}/ocultar")
+    @PatchMapping("/quejas/{id}/ocultar")
     public String ocultarQueja(@PathVariable Long id, RedirectAttributes redirectAttributes) {
         quejaRepo.findById(id).ifPresentOrElse(queja -> {
             queja.setIsVisible(false);
