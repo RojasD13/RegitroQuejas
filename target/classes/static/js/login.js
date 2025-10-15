aa74d8d0562ed8c47e2baefcc5367f3276df77a4
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2025-10-15 17:42:54 -0500

    Eliminación carpeta target

--- target/classes/static/js/login.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const loginButton = document.getElementById('btn-login');
-
-    loginButton.addEventListener('click', function (event) {
-        event.preventDefault();
-        window.location.href = '/registro';
-    });
-});
