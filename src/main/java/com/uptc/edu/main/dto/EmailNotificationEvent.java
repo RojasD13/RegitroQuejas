498ffdf22d42619a9117f7e2f5986f823a226d8f
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2025-11-07 21:11:56 -0500

    docs: se refactoriza EmailProducerService

--- src/main/java/com/uptc/edu/main/dto/EmailNotificationEvent.java
+++ src/main/java/com/uptc/edu/main/dto/EmailNotificationEvent.java
@@ -2,11 +2,13 @@ package com.uptc.edu.main.dto;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class EmailNotificationEvent {
     
     private String userEmail;
--- src/main/java/com/uptc/edu/main/service/EmailProducerService.java
+++ src/main/java/com/uptc/edu/main/service/EmailProducerService.java
@@ -3,41 +3,40 @@ package com.uptc.edu.main.service;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.uptc.edu.main.dto.EmailNotificationEvent;
 import com.uptc.edu.main.kafka.EmailProducer;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+@Service
 public class EmailProducerService {
 
     @Autowired
     private EmailProducer emailProducer;
-    
-    @Autowired
-    private EmailNotificationEvent event;
 
     public void sendNotification(HttpServletRequest request) {
         emailProducer.sendEmailEvent(this.initEvent(request));
     }
 
     private EmailNotificationEvent initEvent(HttpServletRequest request) {
-        event.setUserEmail(getUserEmail(request));
-        event.setClientIp(getClientIp(request));
-        event.setHttpMethod(request.getMethod());
-        event.setRequestUri(request.getRequestURI());
-        event.setTimestamp(LocalDateTime.now());
-        return event;
+        return new EmailNotificationEvent(
+                getUserEmail(request),
+                getClientIp(request),
+                request.getMethod(),
+                request.getRequestURI(),
+                LocalDateTime.now());
     }
 
     private String getUserEmail(HttpServletRequest request) {
-        if (request.getSession().getAttribute("userEmail")==null) {
-            return "Usuario público";        
+        if (request.getSession().getAttribute("userEmail") == null) {
+            return "Usuario público";
         }
         return (String) request.getSession().getAttribute("userEmail");
     }
 
-     private String getClientIp(HttpServletRequest request) {
+    private String getClientIp(HttpServletRequest request) {
         String ip = request.getHeader("X-Forwarded-For");
         if (ip != null && !ip.isBlank()) {
             return ip.split(",")[0].trim();
